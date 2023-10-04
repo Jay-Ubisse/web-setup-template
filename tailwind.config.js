4793669814fb96e57eef10826602d044a5ecbd96
Jay-Ubisse <joaquimubisse@gmail.com>
2023-10-04 19:37:13 +0200

    ADD: Screen sizes to talwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'mobile': '320px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1440px',
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
